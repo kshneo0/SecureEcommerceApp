efa37515eebee93e3c325ec7c70f729244b9d7d4
kshneo0 <kshneo@gmail.com>
2022-02-09 08:53:18 +0900

    sec3 ch22 Getting the paymentIntent - finishing up our handler

--- cmd/api/handlers-api.go
+++ cmd/api/handlers-api.go
@@ -3,6 +3,9 @@ package main
 import (
 	"encoding/json"
 	"net/http"
+	"strconv"
+
+	"github.com/secureEcommerceApp/internal/cards"
 )
 
 type stripePayload struct {
@@ -19,17 +22,56 @@ type jsonResponse struct {
 
 // GetPaymentIntent gets a payment intent, and returns json (or error json)
 func (app *application) GetPaymentIntent(w http.ResponseWriter, r *http.Request) {
-	//임시로 데이터 보내기
-	j := jsonResponse{
-		OK: true,
-	}
+	var payload stripePayload
 
-	out, err := json.MarshalIndent(j, "", "    ")
+	err := json.NewDecoder(r.Body).Decode(&payload)
 	if err != nil {
 		app.errorLog.Println(err)
+		return
 	}
 
-	w.Header().Set("Content-Type", "application/json")
-	w.Write(out)
+	amount, _ := strconv.Atoi(payload.Amount)
+	if err != nil {
+		app.errorLog.Println(err)
+		return
+	}
+
+	card := cards.Card{
+		Secret:   app.config.stripe.secret,
+		Key:      app.config.stripe.key,
+		Currency: payload.Currency,
+	}
+
+	okay := true
+
+	pi, msg, err := card.Charge(payload.Currency, amount)
+	if err != nil {
+		okay = false
+	}
+
+	if okay {
+		out, err := json.MarshalIndent(pi, "", "    ")
+		if err != nil {
+			app.errorLog.Println(err)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(out)
+	} else {
+
+		j := jsonResponse{
+			OK:      false,
+			Message: msg,
+			Content: "",
+		}
+
+		out, err := json.MarshalIndent(j, "", "    ")
+		if err != nil {
+			app.errorLog.Println(err)
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(out)
+	}
 
 }
