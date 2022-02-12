a2d2dc9ae17cff06ded75ec6826a3ce7f1a16dca
kshneo0 <kshneo@gmail.com>
2022-02-12 17:45:05 +0900

    sec7 ch94 Writing middleware on the front end to check authentication

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -5,3 +5,15 @@ import "net/http"
 func SessionLoad(next http.Handler) http.Handler {
 	return session.LoadAndSave(next)
 }
+
+// Auth checks for user authentication status by checking for the key 
+// userID in the session
+func (app *application) Auth(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
+		if !app.Session.Exists(r.Context(), "userID") {
+			http.Redirect(w, r, "/login", http.StatusTemporaryRedirect)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
+}
