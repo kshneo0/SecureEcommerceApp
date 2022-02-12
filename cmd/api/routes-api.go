3d0c2a6eedb30b60ccf0ae8a59d17a61492c89cd
kshneo0 <kshneo@gmail.com>
2022-02-12 16:59:18 +0900

    sec6 ch90 Trying out a protected route

--- cmd/api/routes-api.go
+++ cmd/api/routes-api.go
@@ -28,6 +28,10 @@ func (app *application) routes() http.Handler {
 
 	mux.Route("/api/admin", func(mux chi.Router) {
 		mux.Use(app.Auth)
+
+		mux.Get("/test", func(w http.ResponseWriter, r *http.Request) {
+			w.Write([]byte("got in"))
+		})
 	})
 
 	return mux
