5afbb66c32adf19359639e7a6e68877ac627a9a1
kshneo0 <kshneo@gmail.com>
2022-02-13 09:05:11 +0900

    sec8 ch101 Sending mail Part II

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -24,6 +24,12 @@ type config struct {
 		secret string
 		key    string
 	}
+	smtp struct {
+		host     string
+		port     int
+		username string
+		password string
+	}
 }
 
 type application struct {
@@ -54,6 +60,10 @@ func main() {
 	flag.IntVar(&cfg.port, "port", 4001, "Server port to listen on")
 	flag.StringVar(&cfg.env, "env", "development", "Application environment {development|production|maintenance}")
 	flag.StringVar(&cfg.db.dsn, "dsn", "sanghoonkim:password@tcp(localhost:3306)/widgets?parseTime=true&tls=false", "DSN")
+	flag.StringVar(&cfg.smtp.host, "smtphost", "smtp.mailtrap.io", "smtp host")
+	flag.StringVar(&cfg.smtp.username, "smtpuser", "b1b3810c197ec7", "smtp user")
+	flag.StringVar(&cfg.smtp.password, "smtppass", "1ad4fe5bc864bb", "smtp password")
+	flag.IntVar(&cfg.smtp.port, "smtpport", 587, "smtp port")
 
 	flag.Parse()
 
