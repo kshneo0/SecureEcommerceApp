b209456b4dfff660b038b34893f0b74873c20092
kshneo0 <kshneo@gmail.com>
2022-02-13 20:44:32 +0900

    sec13 ch158 Setting up websockets

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -11,6 +11,8 @@ func (app *application) routes() http.Handler {
 	mux.Use(SessionLoad)
 
 	mux.Get("/", app.Home)
+	mux.Get("/ws", app.WsEndPoint)
+
 	mux.Route("/admin", func(mux chi.Router) {
 		mux.Use(app.Auth)
 		mux.Get("/virtual-terminal", app.VirtualTerminal)
