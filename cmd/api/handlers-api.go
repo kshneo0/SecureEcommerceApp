7a8892b79b686ef84d228fec2c2a33ce9b1f14b7
kshneo0 <kshneo@gmail.com>
2022-02-13 15:23:24 +0900

    sec9 ch121 Listing all Bronze Plan subscribers

--- cmd/api/handlers-api.go
+++ cmd/api/handlers-api.go
@@ -540,6 +540,18 @@ func (app *application) AllSales(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	app.writeJSON(w, http.StatusOK, allSales)
+}
+
+// AllSubscriptions returns all subscriptions as a slice
+func (app *application) AllSubscriptions(w http.ResponseWriter, r *http.Request) {
+
+	//get all sales from database
+	allSales, err := app.DB.GetAllSubscriptions()
+	if err != nil {
+		app.badRequest(w, r, err)
+		return
+	}
 
 	app.writeJSON(w, http.StatusOK, allSales)
 }
