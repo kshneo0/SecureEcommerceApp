1b5d853b8d014eb082a8454c14db6fd78b5fff34
kshneo0 <kshneo@gmail.com>
2022-02-13 20:22:10 +0900

    sec13 ch154 Saving an edited user - part one

--- cmd/api/routes-api.go
+++ cmd/api/routes-api.go
@@ -41,6 +41,7 @@ func (app *application) routes() http.Handler {
 
 		mux.Post("/all-users", app.AllUsers)
 		mux.Post("/all-users/{id}", app.OneUser)
+		mux.Post("/all-users/edit/{id}", app.EditUser)
 
 	})
 
