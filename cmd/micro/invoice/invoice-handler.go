d6e83e535010183f2a6d8e677d7b28b51b88c70b
kshneo0 <kshneo@gmail.com>
2022-02-13 21:34:34 +0900

    sec14 ch163 Receiving data with our micrsoservice

--- cmd/micro/invoice/invoice-handler.go
+++ cmd/micro/invoice/invoice-handler.go
@@ -1,11 +1,46 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
+	"time"
 )
+
+// Order describes the json payload received by this microservice
+type Order struct {
+	ID        int       `json:"id"`
+	Quantity  int       `json:"quantity"`
+	Amount    int       `json:"amount"`
+	Product   string    `json:"product"`
+	FirstName string    `json:"first_name"`
+	LastName  string    `json:"last_name"`
+	Email     string    `json:"email"`
+	CreatedAt time.Time `json:"created_at"`
+}
 
 // CreateAndSendInvoice creates an invoice as a PDF, and emails it to recipient
 func (app *application) CreateAndSendInvoice(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "hello, %s", "world")
+	// receive json
+	var order Order
+
+	err := app.readJSON(w, r, &order)
+	if err != nil {
+		app.badRequest(w, r, err)
+		return
+	}
+
+	// generate a pdf invoice
+
+	// create mail attachment
+
+	// send mail with attachment
+
+	// send reponse
+	var resp struct {
+		Error   bool   `json:"error"`
+		Message string `json:"message"`
+	}
+	resp.Error = false
+	resp.Message = fmt.Sprintf("Invoice %d.pdf created and sent to %s", order.ID, order.Email)
+	app.writeJSON(w, http.StatusCreated, resp)
+
 }
