d0b69f99ace51b3a16ade126500ff4258d7e2f74
kshneo0 <kshneo@gmail.com>
2022-02-13 09:54:54 +0900

    sec8 ch104 Using our urlsigner package

--- cmd/api/handlers-api.go
+++ cmd/api/handlers-api.go
@@ -12,6 +12,7 @@ import (
 	"github.com/go-chi/chi/v5"
 	"github.com/secureEcommerceApp/internal/cards"
 	"github.com/secureEcommerceApp/internal/models"
+	"github.com/secureEcommerceApp/internal/urlsigner"
 	"github.com/stripe/stripe-go/v72"
 )
 
@@ -432,12 +433,32 @@ func (app *application) SendPasswordResetEmail(w http.ResponseWriter, r *http.Re
 		return
 	}
 
+	// verify that email exists
+	_, err = app.DB.GetUserByEmail(payload.Email)
+	if err != nil {
+		var resp struct {
+			Error   bool   `json:"error"`
+			Message string `json:"message"`
+		}
+		resp.Error = true
+		resp.Message = "No matching email found on our system"
+		app.writeJSON(w, http.StatusAccepted, resp)
+		return
+	}
+
+	link := fmt.Sprintf("%s/reset-password?email=%s", app.config.frontend, payload.Email)
+
+	sign := urlsigner.Signer{
+		Secret: []byte(app.config.secretkey),
+	}
+
+	signedLink := sign.GenerateTokenFromString(link)
 
 	var data struct {
 		Link string
 	}
 
-	data.Link = "http://www.unb.ca"
+	data.Link = signedLink
 
 	// send mail
 	err = app.SendMail("info@widgets.com", payload.Email, "Password Reset Request", "password-reset", data)
