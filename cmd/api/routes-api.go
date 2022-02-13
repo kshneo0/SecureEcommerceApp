1ec32cf964aaadc3726f97be2817f5bcc17aa795
kshneo0 <kshneo@gmail.com>
2022-02-13 19:54:33 +0900

    sec13 ch147 Creating a handler and route for all users on the back end

--- cmd/api/routes-api.go
+++ cmd/api/routes-api.go
@@ -39,6 +39,8 @@ func (app *application) routes() http.Handler {
 		mux.Post("/refund", app.RefundCharge)
 		mux.Post("/cancel-subscription", app.CancelSubscription)
 
+		mux.Post("/all-users", app.AllUsers)
+
 	})
 
 	return mux
